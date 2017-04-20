83d2d9be4426128e26362279c5a0d363de8e5ac6
youngdreamer33 <b92.a75@gmail.com>
2017-04-20 15:40:06 +0200

    Push page Profile by Baptiste

---
+++ src/WCS/WildExchangeBundle/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WCS\WildExchangeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function editprofileAction()
+    {
+        return $this->render('WCSWildExchangeBundle:User:editprofile.html.twig');
+
+    }
+}
